8d36c076751ae8d19a72a6ac10dd7eb8a1706777
Ching Chow <ching.chow.sc@gmail.com>
2015-06-23 23:27:40 -0400

    Clean up.

--- pxml.py
+++ pxml.py
@@ -1,4 +1,4 @@
-"""Programmatic XML: Generate XML Programmatically."""
+"""Programmatic XML: Generate XML Programmatically"""
 
 from contextlib import contextmanager
 
@@ -13,66 +13,62 @@ class PXML():
         return "".join(self.raw)
 
     @staticmethod
-    def _verify_str_(obj):
+    def check_str(obj):
         """Raise a TypeError if the given object is not a string."""
         if not isinstance(obj, str):
-            raise TypeError("Type must be a string, but got {}".format(obj))
+            raise TypeError("Expected {}, but got {}".format(type(""), type(obj)))
 
     @staticmethod
-    def _verify_attr_(obj):
+    def check_attr(obj):
         """Raise a TypeError if the given object is not a list of 2-tuple strings."""
         if not isinstance(obj, list):
-            raise TypeError("Type must be a list of 2-tuple strings, but got {}".format(obj))
+            raise TypeError("Expected {}, but got {}".format(type([]), type(obj)))
         for element in obj:
-            if not isinstance(element, tuple) or len(element) != 2:
-                raise TypeError("Type must be a list of 2-tuple strings, but got {}".format(obj))
-        for first, second in obj:
-            if not isinstance(first, str) or not isinstance(second, str):
-                raise TypeError("Type must be a list of 2-tuple strings, but got {}".format(obj))
+            if not isinstance(element, tuple):
+                raise TypeError("Expected {}, but got {}".format(type(()), type(element)))
+            if len(element) != 2:
+                raise TypeError("Expected tuple length to be 2, but got {}".format(len(element)))
+            if not (isinstance(element[0], str) and isinstance(element[1], str)):
+                raise TypeError("Expected all tuple elements to be {}".format(type("")))
 
     @staticmethod
     def attributes(attr):
         """Return the attribute list as a string."""
+        PXML.check_attr(attr)
         to_string = ""
         for name, value in attr:
             to_string += ' {}="{}"'.format(name, value)
         return to_string
 
-    @staticmethod
-    def indent(spaces, depth):
-        """Return the indentation."""
-        return " " * spaces * depth
+    def indent(self):
+        """The current indentation."""
+        return " " * self.spaces * self.depth
 
     @contextmanager
     def tag(self, name, attr=None):
+        PXML.check_str(name)
         if attr is None:
             attr = []
-        PXML._verify_str_(name)
-        PXML._verify_attr_(attr)
-        indent = PXML.indent(self.spaces, self.depth)
-        self.raw.append("{}<{}{}>\n".format(indent, name, PXML.attributes(attr)))
+        self.raw.append("{}<{}{}>\n".format(self.indent(), name, PXML.attributes(attr)))
         self.depth += 1
         yield
         self.depth -= 1
-        self.raw.append("{}</{}>\n".format(indent, name))
+        self.raw.append("{}</{}>\n".format(self.indent(), name))
         return self
 
     @contextmanager
     def itag(self, name, attr=None, indent=True):
+        PXML.check_str(name)
         if attr is None:
             attr = []
-        PXML._verify_str_(name)
-        PXML._verify_attr_(attr)
-        indent = PXML.indent(self.spaces, self.depth) if indent else ""
-        self.raw.append("{}<{}{}>".format(indent, name, PXML.attributes(attr)))
+        self.raw.append("{}<{}{}>".format(self.indent() if indent else "", name, PXML.attributes(attr)))
         yield
         self.raw.append("</{}>".format(name))
         return self
 
     def insert(self, string, indent=True):
-        self._verify_str_(string)
-        indent = PXML.indent(self.spaces, self.depth) if indent else ""
-        self.raw.append("{}{}".format(indent, string))
+        self.check_str(string)
+        self.raw.append("{}{}".format(self.indent() if indent else "", string))
         return self
 
     def newline(self):
--- testpxml.py
+++ testpxml.py
@@ -1,73 +1,72 @@
 import unittest
+from itertools import permutations
 
 from pxml import PXML
 
 
 class TestPXML(unittest.TestCase):
-
     def setUp(self):
         pass
 
     def tearDown(self):
         pass
 
-    def test_type_check(self):
-        types = [None, True, 1, 0.1, 1j, "Hi", "", [], (), {}]
+    def test_check_str(self):
+        types = [None, True, 1, "", "string", [], (), {}]
 
-        ### test _verify_str_ ###
+        # pass: string
+        for element in [t for t in types if isinstance(t, str)]:
+            self.assertIsNone(PXML.check_str(element))
 
-        # no strings
-        with self.assertRaises(TypeError):
-            for element in [x for x in types if not isinstance(x, str)]:
-                PXML._verify_str_(element)
+        # fail: non-string
+        for element in [t for t in types if not isinstance(t, str)]:
+            with self.assertRaises(TypeError):
+                PXML.check_str(element)
 
-        # only strings
-        for element in [x for x in types if isinstance(x, str)]:
-            self.assertIsNone(PXML._verify_str_(element))
+    def test_check_attr(self):
+        types = [None, True, 1, "", "string", [], (), {}]
 
-        ### test _verify_attr_ ###
+        # pass: empty list
+        self.assertIsNone(PXML.check_attr([]))
 
-        # no lists
-        with self.assertRaises(TypeError):
-            for element in [x for x in types if not isinstance(x, list)]:
-                PXML._verify_attr_(element)
+        # pass: list of 2-tuple (tuple elements are strings)
+        for element in permutations([t for t in types if isinstance(t, str)], 2):
+            self.assertIsNone(PXML.check_attr([element]))
 
-        # empty list
-        self.assertIsNone(PXML._verify_attr_([]))
+        # fail: non-list
+        for element in [t for t in types if not isinstance(t, list)]:
+            with self.assertRaises(TypeError):
+                PXML.check_attr(element)
 
-        # 1 element lists
-        with self.assertRaises(TypeError):
-            for element in [[x] for x in types]:
-                PXML._verify_attr_(element)
+        # fail: 1 element list (element is not a 2-tuple of strings)
+        for element in permutations(types, 1):
+            with self.assertRaises(TypeError):
+                PXML.check_attr(list(element))
 
-        # 2 element lists
-        with self.assertRaises(TypeError):
-            for element in [[x, y] for x in types for y in types]:
-                PXML._verify_attr_(element)
+        # fail: 2 element list (elements are not a 2-tuple of strings)
+        for element in permutations(types, 2):
+            with self.assertRaises(TypeError):
+                PXML.check_attr(list(element))
 
-        # 3 element lists
-        with self.assertRaises(TypeError):
-            for element in [[x, y, z] for x in types for y in types for z in types]:
-                PXML._verify_attr_(element)
+        # fail: 3 element list (elements are not a 2-tuple of strings)
+        for element in permutations(types, 3):
+            with self.assertRaises(TypeError):
+                PXML.check_attr(list(element))
 
-        # 1-tuple lists
-        with self.assertRaises(TypeError):
-            for element in [[(x,)] for x in types]:
-                PXML._verify_attr_(element)
+        # fail: list of 1-tuple
+        for element in permutations(types, 1):
+            with self.assertRaises(TypeError):
+                PXML.check_attr([element])
 
-        # 2-tuple lists, no strings
-        with self.assertRaises(TypeError):
-            for element in [[(x, y)] for x in types if not isinstance(x, str) for y in types if not isinstance(y, str)]:
-                PXML._verify_attr_(element)
+        # fail: list of 2-tuple (tuple elements are non-strings)
+        for element in permutations([t for t in types if not isinstance(t, str)], 2):
+            with self.assertRaises(TypeError):
+                PXML.check_attr([element])
 
-        # 3-tuple lists
-        with self.assertRaises(TypeError):
-            for element in [[(x, y, z)] for x in types for y in types for z in types]:
-                PXML._verify_attr_(element)
-
-        # 2-tuple lists, only strings
-        for element in [[(x, y)] for x in types if isinstance(x, str) for y in types if isinstance(y, str)]:
-            self.assertIsNone(PXML._verify_attr_(element))
+        # fail: list of 3-tuple
+        for element in permutations(types, 3):
+            with self.assertRaises(TypeError):
+                PXML.check_attr([element])
 
     def test_attributes(self):
         self.assertEqual('', PXML.attributes([]))
@@ -76,74 +75,72 @@ class TestPXML(unittest.TestCase):
         self.assertEqual(' a="1" b="2" c="3"', PXML.attributes([("a", "1"), ("b", "2"), ("c", "3")]))
 
     def test_indent(self):
+        pxml = PXML()
+
         for spaces in range(5):
+            pxml.spaces = spaces
             for depth in range(5):
-                self.assertEqual(" " * spaces * depth, PXML.indent(spaces, depth))
+                pxml.depth = depth
+                self.assertEqual(" " * spaces * depth, pxml.indent())
 
     def test_insert(self):
         pxml = PXML()
 
         pxml.insert("Hello")
-        self.assertEqual(pxml.raw, ["Hello"])
-        self.assertEqual(str(pxml), "Hello")
+        self.assertEqual(["Hello"], pxml.raw)
+        self.assertEqual("Hello", str(pxml))
 
         pxml.insert("World")
-        self.assertEqual(pxml.raw, ["Hello", "World"])
-        self.assertEqual(str(pxml), "HelloWorld")
+        self.assertEqual(["Hello", "World"], pxml.raw)
+        self.assertEqual("HelloWorld", str(pxml))
 
         pxml.depth = 1
         pxml.insert("One")
-        one = PXML.indent(pxml.spaces, pxml.depth)
-        self.assertEqual(pxml.raw, ["Hello", "World", one + "One"])
-        self.assertEqual(str(pxml), "HelloWorld{}One".format(one))
+        one = pxml.indent()
+        self.assertEqual(["Hello", "World", one + "One"], pxml.raw)
+        self.assertEqual("HelloWorld{}One".format(one), str(pxml))
 
         pxml.depth = 2
         pxml.insert("Two")
-        two = PXML.indent(pxml.spaces, pxml.depth)
-        self.assertEqual(pxml.raw, ["Hello", "World", one + "One", two + "Two"])
-        self.assertEqual(str(pxml), "HelloWorld{}One{}Two".format(one, two))
+        two = pxml.indent()
+        self.assertEqual(["Hello", "World", one + "One", two + "Two"], pxml.raw)
+        self.assertEqual("HelloWorld{}One{}Two".format(one, two), str(pxml))
 
         pxml.depth = 3
         pxml.insert("Three")
-        three = PXML.indent(pxml.spaces, pxml.depth)
-        self.assertEqual(pxml.raw, ["Hello", "World", one + "One", two + "Two", three + "Three"])
-        self.assertEqual(str(pxml), "HelloWorld{}One{}Two{}Three".format(one, two, three))
+        three = pxml.indent()
+        self.assertEqual(["Hello", "World", one + "One", two + "Two", three + "Three"], pxml.raw)
+        self.assertEqual("HelloWorld{}One{}Two{}Three".format(one, two, three), str(pxml))
 
         pxml.depth = 0
         pxml.insert("Bye").insert("Bye")
-        self.assertEqual(pxml.raw, ["Hello", "World", one + "One", two + "Two", three + "Three", "Bye", "Bye"])
-        self.assertEqual(str(pxml), "HelloWorld{}One{}Two{}ThreeByeBye".format(one, two, three))
+        self.assertEqual(["Hello", "World", one + "One", two + "Two", three + "Three", "Bye", "Bye"], pxml.raw)
+        self.assertEqual("HelloWorld{}One{}Two{}ThreeByeBye".format(one, two, three), str(pxml))
 
     def test_newline(self):
         pxml = PXML()
 
         pxml.newline()
-        self.assertEqual(pxml.raw, ["\n"])
-        self.assertEqual(str(pxml), "\n")
+        self.assertEqual(["\n"], pxml.raw)
+        self.assertEqual("\n", str(pxml))
 
         pxml.newline()
-        self.assertEqual(pxml.raw, ["\n", "\n"])
-        self.assertEqual(str(pxml), "\n\n")
+        self.assertEqual(["\n", "\n"], pxml.raw)
+        self.assertEqual("\n\n", str(pxml))
 
-        pxml.depth = 1
-        pxml.newline()
-        self.assertEqual(pxml.raw, ["\n", "\n", "\n"])
-        self.assertEqual(str(pxml), "\n\n\n")
-
-        pxml.depth = 2
-        pxml.newline()
-        self.assertEqual(pxml.raw, ["\n", "\n", "\n", "\n"])
-        self.assertEqual(str(pxml), "\n\n\n\n")
-
-        pxml.depth = 3
-        pxml.newline()
-        self.assertEqual(pxml.raw, ["\n", "\n", "\n", "\n", "\n"])
-        self.assertEqual(str(pxml), "\n\n\n\n\n")
-
-        pxml.depth = 0
         pxml.newline().newline()
-        self.assertEqual(pxml.raw, ["\n", "\n", "\n", "\n", "\n", "\n", "\n"])
-        self.assertEqual(str(pxml), "\n\n\n\n\n\n\n")
+        self.assertEqual(["\n", "\n", "\n", "\n"], pxml.raw)
+        self.assertEqual("\n\n\n\n", str(pxml))
+
+    def test_itag(self):
+        # <b>HelloWorld</b>
+        # <b><i>HelloWorld</i></b>
+        # <b><i><u>HelloWorld</u></i></b>
+        # <b>Hello</b><b>World</b>
+        # <span id="Hello">Hello</span>
+        # <span id="Hello"><span id="World">HelloWorld</span></span>
+        # <span>Hello</span><span>World</span>
+        pass
 
 
 if __name__ == "__main__":
