8f6d2d61c29eb40965b310efb10d1f3b77c1aa27
Ching Chow <ching.chow.sc@gmail.com>
2018-03-16 19:25:26 -0400

    Cleanup and consolidation

--- src/pxml/pxml.py
+++ src/pxml/pxml.py
@@ -1,57 +1,83 @@
 """Programmatic XML: Write XML Programmatically"""
 
 from contextlib import contextmanager
+from typing import Generator, List, Tuple, Union
 
-from .attr import Attr  # pylint: disable=E
-from .error import NonVoidError, VoidError  # pylint: disable=E
-from .typehint import FormattedElements, Wrapper  # pylint: disable=E
+
+BooleanAttribute = str
+ValueAttribute = Tuple[str, Union[int, str]]  # pylint: disable=C
+Attribute = Union[BooleanAttribute, ValueAttribute]  # pylint: disable=C
 
 
 class PXML():
     """Create HTML Elements.
     """
     @staticmethod
-    def _open_tag(name: str, attr: Attr = None) -> str:
-        return f"<{name}{'' if attr is None else f' {Attr(*attr)}'}>"
+    def _close_tag(elem: str) -> str:
+        return f"</{elem}>"
 
     @staticmethod
-    def _close_tag(name: str) -> str:
-        return f"</{name}>"
-
-    @staticmethod
-    def _is_void(name: str) -> bool:
+    def _is_void(elem: str) -> bool:
         # http://w3c.github.io/html/syntax.html#void-elements
-        return name in ["area", "base", "br", "col", "embed", "hr", "img", "input", "link", "meta", "param", "source", "track", "wbr"]  # pylint: disable=C
+        return elem in ["area", "base", "br", "col", "embed", "hr", "img", "input", "link", "meta", "param", "source", "track", "wbr"]  # pylint: disable=C
+
+    @staticmethod
+    def _open_tag(elem: str, attrs: str = "") -> str:
+        return f"<{elem}{'' if not attrs else f' {attrs}'}>"
+
+    @staticmethod
+    def attr(*attrs: Attribute) -> str:
+        """Return a string formatted as HTML attributes.
+
+        Can take multiple strings and 2-tuples.  Strings are treated as boolean attributes and
+        2-tuples are treated as value attributes.  Tuples take the form (str, str) or (str, int).
+
+        For example, to create the attributes in the element:
+        <video src="videofile.webm" autoplay width="800" height="600"></video>
+
+        `attr(("src", "videofile.webm"), autoplay, ("width", 800), ("height", 600))`
+        """
+        formatted = []
+        for attr_ in attrs:
+            if isinstance(attr_, str):
+                formatted.append(attr_)
+            elif isinstance(attr_, tuple) and len(attr_) == 2:
+                formatted.append(f'{attr_[0]}="{attr_[1]}"')
+            else:
+                raise ValueError(f"Bad attribute: {attr_}")
+        return " ".join(formatted)
 
     def __init__(self, spaces: int = 4) -> None:
         self.depth: int = 0
-        self.elems: FormattedElements = []
+        self.elems: List[str] = []
         self.oneline: bool = False
         self.spaces: int = spaces
+
+    def __contains__(self, item: object) -> bool:
+        return str(item) in str(self)
 
     def __eq__(self, other: object) -> bool:
         return str(self) == str(other)
 
+    def __len__(self) -> int:
+        return len(str(self))
+
     def __repr__(self) -> str:
-        # TODO: REPR
-        pass
+        return "".join(self.elems)
 
     def __str__(self) -> str:
         return "".join(self.elems)
 
-    def write(self, string: str) -> "PXML":
-        """doc"""  # TODO: Doc
-        if string:
+    def append(self, string: str) -> "PXML":
+        """Add a string."""
+        if isinstance(string, str):
             if not self.oneline:
                 self.indent()
             self.elems.append(string)
             if not self.oneline:
                 self.newline()
-        return self
-
-    def newline(self) -> "PXML":
-        """Add a newline."""
-        self.elems.append("\n")
+        else:
+            raise ValueError("Value being appended must be a string type")
         return self
 
     def indent(self) -> "PXML":
@@ -59,31 +85,46 @@ class PXML():
         self.elems.append(" " * self.spaces * self.depth)
         return self
 
-    def vwrap(self, name: str, attr: Attr = None) -> "PXML":
+    def newline(self) -> "PXML":
+        """Add a newline."""
+        self.elems.append("\n")
+        return self
+
+    def vwrap(self, elem: str, attrs: str = "") -> "PXML":
         """Add a void element."""
-        if not self._is_void(name):
-            raise NonVoidError(name)
-        self.write(self._open_tag(name, attr))
+        if not self._is_void(elem):
+            raise ValueError(f"Use the wrap or owrap method for non-void elements like {elem}")
+        self.append(self._open_tag(elem, attrs))
         return self
 
     @contextmanager
-    def owrap(self, name: str, attr: Attr = None) -> Wrapper:
-        """Add an HTML element (oneliner)."""
-        if self._is_void(name):
-            raise VoidError(name)
-        self.oneline = True
-        self.write(self._open_tag(name, attr))
+    def owrap(self, elem: str, attrs: str = "", newline: bool = False) -> Generator:
+        """Add an element (oneliner)."""
+        if self._is_void(elem):
+            raise ValueError(f"Use the vwrap method for void elements like {elem}")
+        if not self.oneline:
+            self.oneline = True
+            self.indent()
+        self.append(self._open_tag(elem, attrs))
         yield
-        self.write(self._close_tag(name))
+        self.append(self._close_tag(elem))
+        if newline:
+            self.newline()
+            self.oneline = False
 
     @contextmanager
-    def wrap(self, name: str, attr: Attr = None) -> Wrapper:
-        """Add an HTML element."""
-        if self._is_void(name):
-            raise VoidError(name)
-        self.oneline = False
-        self.write(self._open_tag(name, attr))
+    def wrap(self, elem: str, attrs: str = "") -> Generator:
+        """Add an element."""
+        if self._is_void(elem):
+            raise ValueError(f"Use the vwrap method for void elements like {elem}")
+        if self.oneline:
+            self.oneline = False
+            self.newline()
+        self.append(self._open_tag(elem, attrs))
         self.depth += 1
         yield
+        if self.oneline:
+            self.oneline = False
+            self.newline()
         self.depth -= 1
-        self.write(self._close_tag(name))
+        self.append(self._close_tag(elem))
--- tests/test_pxml.py
+++ tests/test_pxml.py
@@ -1,163 +1,211 @@
-"""Tests"""
+# pylint: skip-file
+
+import sys
+from pathlib import Path
 
 import pytest
 
+HERE = Path(__file__).resolve().parent
+sys.path.append(str(HERE.parent.joinpath("src")))
 
-def test_write(PXML):
-    pxml = PXML()
-    pxml.depth = 0
-    pxml.write("a")
-    assert str(pxml) == "a\n"
-
-    pxml = PXML()
-    pxml.depth = 1
-    pxml.write("a")
-    assert str(pxml) == "    a\n"
+from pxml.pxml import PXML
 
 
-def test_vwrap(PXML):
-    """test"""
-    pxml = PXML()
-    pxml.vwrap("br")
-    assert str(pxml) == "<br>\n"
-
-    pxml = PXML()
-    pxml.vwrap("img", [("src", "/world.png"), ("width", "640"), ("height", "480")])
-    assert str(pxml) == '<img src="/world.png" width="640" height="480">\n'
+def _read(filename):
+    with open(HERE.joinpath(filename), "r") as f:
+        return f.read()
 
 
-@pytest.mark.parametrize("spaces", range(5))
-@pytest.mark.parametrize("depth", range(5))
-def test_indent(PXML, spaces, depth):
-    """test"""
-    pxml = PXML(spaces)
-    pxml.depth = depth
-    pxml.indent()
-    assert str(pxml) == " " * pxml.spaces * pxml.depth
+@pytest.fixture
+def pxml():
+    return PXML()
 
 
-def test_newline(PXML):
-    """test"""
-    pxml = PXML()
-    pxml.newline()
-    assert str(pxml) == "\n"
-    pxml.newline()
-    assert str(pxml) == "\n\n"
+class TestDunder():
+    def test_dunder(self, pxml):
+        assert not len(pxml)
+        pxml.append("a")
+        assert "a" in pxml
+        assert len(pxml) == 2  # + 1 due to newline
+        assert str(pxml) == repr(pxml) == pxml == PXML().append("a") == "a\n"
 
 
-def test_owrap(PXML):
-    """test"""
-    pxml = PXML()
-    with pxml.owrap("b"):
-        pxml.write("HelloWorld")
-    assert str(pxml) == "<b>HelloWorld</b>"
+class TestAttribute():
+    bad_in = [None, True, 1, [], {}, (), ("a",), ("a", "b", "c")]  # non-strings and len(tuple) != 2
+    good_in = [["a"], ["a", "b"], [("a", 1)], [("a", 1), ("b", "2")], ["a", ("b", 2)], [("a", 1), "b"]]
+    good_out = ["a", "a b", 'a="1"', 'a="1" b="2"', 'a b="2"', 'a="1" b', 1]
 
-    pxml = PXML()
-    with pxml.owrap("b"):
-        with pxml.owrap("i"):
-            pxml.write("HelloWorld")
-    assert str(pxml) == "<b><i>HelloWorld</i></b>"
+    def test_empty_input(self):
+        assert PXML.attr() == ""
 
-    pxml = PXML()
-    with pxml.owrap("b"):
-        with pxml.owrap("i"):
-            with pxml.owrap("u"):
-                pxml.write("HelloWorld")
-    assert str(pxml) == "<b><i><u>HelloWorld</u></i></b>"
+    @pytest.mark.parametrize("test_input, expected", list(zip(good_in, good_out)))
+    def test_good_input(self, test_input, expected):
+        assert PXML.attr(*test_input) == expected
 
-    pxml = PXML()
-    with pxml.owrap("b"):
-        pxml.write("Hello")
-    with pxml.owrap("b"):
-        pxml.write("World")
-    assert str(pxml) == "<b>Hello</b><b>World</b>"
-
-    pxml = PXML()
-    with pxml.owrap("span", [("id", "Hello")]):
-        pxml.write("Hello")
-    assert str(pxml) == '<span id="Hello">Hello</span>'
-
-    pxml = PXML()
-    with pxml.owrap("span", [("id", "Hello")]):
-        with pxml.owrap("span", [("id", "World")]):
-            pxml.write("HelloWorld")
-    assert str(pxml) == '<span id="Hello"><span id="World">HelloWorld</span></span>'
+    @pytest.mark.parametrize("bad", bad_in)
+    def test_bad_input(self, bad):
+        with pytest.raises(ValueError):
+            PXML.attr(bad)
 
 
-def test_wrap(PXML):
-    """
-    <b>
-        HelloWorld
-    </b>
-    """
-    pxml = PXML(4)
-    with pxml.wrap("b"):
-        pxml.write("HelloWorld")
-    assert str(pxml) == "<b>\n    HelloWorld\n</b>\n"
+class TestAppend():
+    @pytest.mark.parametrize("repeat", range(4))
+    def test_with_newline(self, pxml, repeat):
+        for _ in range(repeat):
+            pxml.append("a")
+        assert pxml == "a\n" * repeat
 
-    """
-    <b>
-        <i>
-            HelloWorld
-        </i>
-    </b>
-    """
-    pxml = PXML(4)
-    with pxml.wrap("b"):
-        with pxml.wrap("i"):
-            pxml.write("HelloWorld")
-    assert str(pxml) == "<b>\n    <i>\n        HelloWorld\n    </i>\n</b>\n"
+    @pytest.mark.parametrize("repeat", range(4))
+    def test_without_newline(self, pxml, repeat):
+        pxml.oneline = True
+        for _ in range(repeat):
+            pxml.append("a")
+        assert pxml == "a" * repeat
 
-    """
-    <b>
-        <i>
-            <u>
-                HelloWorld
-            </u>
-        </i>
-    </b>
-    """
-    pxml = PXML(4)
-    with pxml.wrap("b"):
-        with pxml.wrap("i"):
-            with pxml.wrap("u"):
-                pxml.write("HelloWorld")
-    assert str(pxml) == "<b>\n    <i>\n        <u>\n            HelloWorld\n        </u>\n    </i>\n</b>\n"
+    @pytest.mark.parametrize("bad", [None, True, 1])
+    def test_non_string_type(self, pxml, bad):
+        with pytest.raises(ValueError):
+            pxml.append(bad)
 
-    """
-    <b>
-        Hello
-    </b>
-    <b>
-        World
-    </b>
-    """
-    pxml = PXML(4)
-    with pxml.wrap("b"):
-        pxml.write("Hello")
-    with pxml.wrap("b"):
-        pxml.write("World")
-    assert str(pxml) == "<b>\n    Hello\n</b>\n<b>\n    World\n</b>\n"
 
-    """
-    <span id="Hello">
-        Hello
-    </span>
-    """
-    pxml = PXML(4)
-    with pxml.wrap("span", [("id", "Hello")]):
-        pxml.write("Hello")
-    assert str(pxml) == '<span id="Hello">\n    Hello\n</span>\n'
+class TestIndent():
+    @pytest.mark.parametrize("spaces", range(4))
+    @pytest.mark.parametrize("depth", range(4))
+    def test_depth(self, pxml, spaces, depth):
+        pxml.spaces = spaces
+        pxml.depth = depth
+        assert pxml.indent() == " " * pxml.spaces * pxml.depth
 
-    """
-    <span id="Hello">
-        <span id="World">
-            HelloWorld
-        </span>
-    </span>
-    """
-    pxml = PXML(4)
-    with pxml.wrap("span", [("id", "Hello")]):
-        with pxml.wrap("span", [("id", "World")]):
-            pxml.write("HelloWorld")
-    assert str(pxml) == '<span id="Hello">\n    <span id="World">\n        HelloWorld\n    </span>\n</span>\n'
+
+class TestNewline():
+    @pytest.mark.parametrize("depth", range(2))
+    def test_depth_does_not_indent(self, pxml, depth):
+        pxml.depth = depth
+        pxml.newline().newline()
+        assert pxml == "\n\n"
+        pxml.newline().newline()
+        assert " " not in pxml
+
+
+class TestVoidElement():
+    def test_without_attribute(self, pxml):
+        pxml.vwrap("br")
+        assert pxml == "<br>\n"
+
+    def test_with_attribute(self, pxml):
+        pxml.vwrap("img", PXML.attr("abc", ("x", 1), ("y", 2), ("z", 3)))
+        assert pxml == '<img abc x="1" y="2" z="3">\n'
+
+    @pytest.mark.parametrize("bad", ["a", "b", "c"])
+    def test_non_void_element(self, pxml, bad):
+        with pytest.raises(ValueError):
+            pxml.vwrap(bad)
+
+
+class TestOneline():
+    def test_single(self, pxml):
+        with pxml.owrap("a"):
+            pxml.append("1")
+        assert pxml == "<a>1</a>"
+
+    def test_nested(self, pxml):
+        with pxml.owrap("a"), pxml.owrap("b"):
+            pxml.append("1 2")
+        assert pxml == "<a><b>1 2</b></a>"
+
+    def test_double_nested(self, pxml):
+        with pxml.owrap("a"), pxml.owrap("b"), pxml.owrap("c"):
+            pxml.append("1 2 3")
+        assert pxml == "<a><b><c>1 2 3</c></b></a>"
+
+    def test_side_by_side(self, pxml):
+        with pxml.owrap("a"):
+            pxml.append("1")
+        with pxml.owrap("b"):
+            pxml.append("2")
+        assert pxml == "<a>1</a><b>2</b>"
+
+    def test_nested_side_by_side(self, pxml):
+        with pxml.owrap("a"):
+            with pxml.owrap("b"):
+                pxml.append("2")
+            with pxml.owrap("c"):
+                pxml.append("3")
+        assert pxml == "<a><b>2</b><c>3</c></a>"
+
+
+class TestWrap():
+    def test_single(self, pxml):
+        with pxml.wrap("a"):
+            pxml.append("1")
+        assert pxml == _read("expected_single.txt")
+
+    def test_nested(self, pxml):
+        with pxml.wrap("a"), pxml.wrap("b"):
+            pxml.append("1 2")
+        assert pxml == _read("expected_nested.txt")
+
+    def test_double_nested(self, pxml):
+        with pxml.wrap("a"), pxml.wrap("b"), pxml.wrap("c"):
+            pxml.append("1 2 3")
+        assert pxml == _read("expected_double_nested.txt")
+
+    def test_side_by_side(self, pxml):
+        with pxml.wrap("a"):
+            pxml.append("1")
+        with pxml.wrap("b"):
+            pxml.append("2")
+        assert pxml == _read("expected_side_by_side.txt")
+
+    def test_nested_side_by_side(self, pxml):
+        with pxml.wrap("a"):
+            with pxml.wrap("b"):
+                pxml.append("2")
+            with pxml.wrap("c"):
+                pxml.append("3")
+        assert pxml == _read("expected_nested_side_by_side.txt")
+
+    def test_nested_oneline(self, pxml):
+        with pxml.wrap("a"):
+            with pxml.owrap("b"):
+                pxml.append("2")
+        assert pxml == _read("expected_nested_oneline.txt")
+
+    def test_nested_oneline_side_by_side(self, pxml):
+        with pxml.wrap("a"):
+            with pxml.owrap("b", newline=True):
+                pxml.append("2")
+            with pxml.owrap("c"):
+                pxml.append("3")
+        assert pxml == _read("expected_nested_oneline_side_by_side.txt")
+
+    def test_complex(self, pxml):
+        with pxml.wrap("a"):
+            pxml.append("1")
+            pxml.newline()
+            with pxml.wrap("b"):
+                pxml.append("2")
+            pxml.newline()
+            with pxml.owrap("c", newline=True):
+                pxml.append("3")
+            with pxml.owrap("d"):
+                pxml.append("4")
+            pxml.newline()
+            with pxml.wrap("e"):
+                pxml.append("5")
+                pxml.newline()
+            with pxml.owrap("f"):
+                pxml.append("6")
+            pxml.newline()
+            with pxml.wrap("g"):
+                with pxml.wrap("h"):
+                    pxml.append("8")
+                    with pxml.owrap("i"):
+                        pxml.append("9")
+                    with pxml.owrap("j", newline=True):
+                        pxml.append("10")
+                    pxml.newline()
+                    with pxml.owrap("k", newline=True), pxml.owrap("l"):
+                        pxml.append("11 12")
+                pxml.append("7")
+        assert pxml == _read("expected_complex.txt")
